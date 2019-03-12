5a1ba85115a3367b45b72fadb15fbfa1044fc27a
Einar Jonsson <einarjonsson@mac.com>
2019-03-12 16:26:00 +0100

    beginning of profile show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
   end
 
   def show
+    @user = current_user
   end
 
   def new
