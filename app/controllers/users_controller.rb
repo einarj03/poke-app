7e69d6e26cbb96fa1857c26fa23b861edcff3736
Victoria Kempter <victoria@thekempters.de>
2019-03-12 15:35:16 +0100

    added index and customized view & scss

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
