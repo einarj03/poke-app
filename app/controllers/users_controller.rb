9d97a1f0cd67423a607a81da50ce7866cc19bdac
Darrick Ervin II <sherwood_3494@yahoo.com>
2019-03-12 17:13:31 +0100

    user edit and update controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,16 +19,24 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :gender,
-    :date_of_birth, :search_gender, :interest_gender)
+    :date_of_birth, :search_gender, :interest_gender, :photo)
   end
 
 end
