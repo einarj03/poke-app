409cf20676cc8c8a1be48f56d4353b76add5fbed
Darrick Ervin II <sherwood_3494@yahoo.com>
2019-03-13 00:03:32 +0100

    message controller complete and message index. Index needs front end flare

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,15 @@
+class MessagesController < ApplicationController
+
+  def index
+    @messages = Message.where(poke_id = params[:poke_id])
+  end
+
+  def create
+    @message = Message.new(user_params)
+    if @user.save
+    else
+      render :index
+    end
+  end
+
+end
