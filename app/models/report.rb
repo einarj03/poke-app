80ccfc01c5a05681a1d4cc5a270c31304efd2226
Darrick Ervin II <sherwood_3494@yahoo.com>
2019-03-12 14:41:03 +0100

    models for block and report

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,6 @@
 class Report < ApplicationRecord
-  belongs_to :reporter, class_name: "Users", foreign_key: "reporter_id"
-  belongs_to :reportee, class_name: "Users", foreign_key: "reportee_id"
+  belongs_to :reporter, class_name: "User", foreign_key: "reporter_id"
+  belongs_to :reportee, class_name: "User", foreign_key: "reportee_id"
 
   validates :description, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_03_12_111541) do
+ActiveRecord::Schema.define(version: 2019_03_12_120540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "blocks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -30,7 +28,8 @@ ActiveRecord::Schema.define(version: 2019_03_12_111541) do
     t.integer "reporter_id"
     t.integer "reportee_id"
     t.text "description"
-=======
+  end
+
   create_table "suggestions", force: :cascade do |t|
     t.text "content"
     t.string "for"
@@ -46,7 +45,6 @@ ActiveRecord::Schema.define(version: 2019_03_12_111541) do
     t.datetime "updated_at", null: false
     t.index ["suggestion_id"], name: "index_user_profile_questions_on_suggestion_id"
     t.index ["user_id"], name: "index_user_profile_questions_on_user_id"
->>>>>>> master
   end
 
   create_table "users", force: :cascade do |t|
