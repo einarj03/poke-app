513de03b6baf266979cb06aebf22184d4d43117d
Darrick Ervin II <sherwood_3494@yahoo.com>
2019-03-12 16:15:47 +0100

    user create and new plus view done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,16 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -16,4 +23,12 @@ class UsersController < ApplicationController
 
   def update
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :gender,
+    :date_of_birth, :search_gender, :interest_gender)
+  end
+
 end
