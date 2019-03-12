b78f982fb6f2d2aad69a57475c306de00bb2b18c
Einar Jonsson <einarjonsson@mac.com>
2019-03-12 11:52:19 +0100

    created a user model with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :date_of_birth, presence: true
+  validates :gender, presence: true
+  validate :positive_age
+
+  def positive_age
+    date_of_birth < Date.today
+  end
 end
