f74ba18866ca9e6260be6e95cb6254a6aa0f9723
Einar Jonsson <einarjonsson@mac.com>
2019-03-12 12:03:56 +0100

    mounted a photo uploader to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_102848) do
+ActiveRecord::Schema.define(version: 2019_03_12_110245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_03_12_102848) do
     t.float "longitude"
     t.float "latitude"
     t.date "location_updated_at"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
