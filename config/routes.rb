6061b33b1157da941ff53e187d9c30c01c5cd926
Ferdinand Nussbaum <ferdinand18.2n@gmail.com>
2019-03-13 10:49:35 +0100

    Not finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, except: [:destroy] do
-    resources :pokes, only: [:new, :create]
+    resources :pokes, only: [:new, :create, :show, :index]
     resources :block, only: [:new, :create]
     resources :report, only: [:new, :create]
     resources :user_profile_questions, only: [:create]
   end
 
   resources :user_profile_questions, only: [:update, :destroy]
-
-  resources :pokes, only: [:index] do
-    resources :messages, only: [:index, :create]
-  end
 end
