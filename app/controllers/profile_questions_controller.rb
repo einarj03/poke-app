ae0f69965c745b8454350768453bd32cbf6ef364
Ferdinand Nussbaum <ferdinand18.2n@gmail.com>
2019-03-14 11:40:05 +0100

    Setting up the question controller

--- app/controllers/profile_questions_controller.rb
+++ app/controllers/profile_questions_controller.rb
@@ -1,10 +1,22 @@
 class ProfileQuestionsController < ApplicationController
   def create
+    @profile_question = Profile.new(rest_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
+
+  private
+
 end
