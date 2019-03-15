5d2b64857c0c607fe4785261f7edf469e6f70bf1
Einar Jonsson <einarjonsson@mac.com>
2019-03-15 15:58:42 +0100

    fixed time left function

--- app/models/poke.rb
+++ app/models/poke.rb
@@ -21,6 +21,23 @@ class Poke < ApplicationRecord
     end
   end
 
+  def time_left
+    t = DateTime.now + 12.hours - created_at.to_datetime
+    hours = (t * 24).to_i
+    minutes = (t * 24 * 60).to_i
+    seconds = (t * 24 * 60 * 60).to_i
+    if hours.positive?
+      output = "#{hours}h left"
+      return output
+    elsif minutes.positive?
+      output = "#{minutes}m left"
+      return output
+    else
+      output = "#{seconds}s left"
+      return output
+    end
+  end
+
   # def time_left
   #   m = Time.now + 12.hours - self.created_at
   #   Time.at(m.strftime("%lh%Mm%Ss")
