7f82db2f81748b4ab8d5c5f3e42aae054e1e44b4
Ferdinand Nussbaum <ferdinand18.2n@gmail.com>
2019-03-18 16:51:40 +0100

    Integrate profile answers in flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,22 +9,6 @@ class UsersController < ApplicationController
     @report = Report.new
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to users_path
-    else
-      render :new
-    end
-  end
-
-  def edit
-  end
-
   def update
     if @user.update(user_params)
       redirect_to user_path(@user)
