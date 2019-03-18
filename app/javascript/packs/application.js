7c23640c1a4e881b1061c19957d6dd5ded3ff6ba
Einar Jonsson <einarjonsson@mac.com>
2019-03-18 21:23:54 +0100

    message box now auto grows

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,6 @@
 import "bootstrap";
 
+import { initAutoResizeTextArea } from '../components/initAutoResizeTextArea';
 import { initPokeSuggestions } from '../components/input';
 initPokeSuggestions();
+initAutoResizeTextArea();
