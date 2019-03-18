db2efb33020f94eadf798e6b274a8f5242336bc1
Einar Jonsson <einarjonsson@mac.com>
2019-03-18 17:43:59 +0100

    poke suggestions now also get inputted inoto the poke show page

--- app/javascript/components/input.js
+++ app/javascript/components/input.js
@@ -1,7 +1,7 @@
 
 const initPokeSuggestions = () => {
   const pokeSuggestions = document.querySelectorAll('.poke-suggestions-content-container')
-  const pokeContent = document.querySelector('#poke_content')
+  const pokeContent = document.querySelector('.message-content')
   pokeSuggestions.forEach((poke_suggestion) => {
     poke_suggestion.addEventListener('click', (event) => {
       pokeContent.value = event.target.innerText
