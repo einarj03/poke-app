927436645cd2e7e2076ff354fd8ebf3afc10691a
Einar Jonsson <einarjonsson@mac.com>
2019-03-19 10:20:00 +0100

    renamed input to initPokeSuggestions

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,6 @@
 import "bootstrap";
 
 import { initAutoResizeTextArea } from '../components/initAutoResizeTextArea';
-import { initPokeSuggestions } from '../components/input';
+import { initPokeSuggestions } from '../components/initPokeSuggestions';
 initPokeSuggestions();
 initAutoResizeTextArea();
