26774fb9c7749d39553d83943bd9367d686a71f5
Einar Jonsson <einarjonsson@mac.com>
2019-03-19 13:29:22 +0100

    removed user controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: [:show, :edit, :update]
+  before_action :find_user, only: [:show]
 
   def index
     @users = User.all
@@ -7,24 +7,6 @@ class UsersController < ApplicationController
 
   def show
     @report = Report.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to user_path(@user)
-    else
-      render :new
-    end
-  end
-
-  def update
-    if @user.update(user_params)
-      redirect_to user_path(@user)
-    else
-      render :edit
-    end
-
   end
 
   private
