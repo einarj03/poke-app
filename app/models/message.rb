90a6a341aa964ee6b6c5966a96ae64a2e01f6fa4
Einar Jonsson <einarjonsson@mac.com>
2019-03-19 18:29:55 +0100

    messages automatically appear on both users screens using actioncable

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,7 +4,18 @@ class Message < ApplicationRecord
 
   validates :content, presence: true
 
+  after_create :broadcast_message
+
   def from?(some_user)
     some_user == user
+  end
+
+  def broadcast_message
+    ActionCable.server.broadcast("chat_room_#{poke.id}", {
+      message_partial: ApplicationController.renderer.render(
+        partial: "messages/message",
+        locals: { message: self, user_is_messages_author: false }),
+      current_user_id: user.id
+    })
   end
 end
