18dbb69ca6556c3b0a1a7a57ae7bea6f753138bf
Einar Jonsson <einarjonsson@mac.com>
2019-03-20 18:21:25 +0100

    three tabs now instead of two

--- app/javascript/components/initPokesIndexTabs.js
+++ app/javascript/components/initPokesIndexTabs.js
@@ -1,16 +1,21 @@
 const tabs = document.querySelectorAll('.tab');
+const sentPokes = document.querySelector('.sent-pokes');
+const receivedPokes = document.querySelector('.received-pokes')
+const connectionsPokes = document.querySelector('.connections-pokes')
 const pokeContainers = document.querySelectorAll('.poke-container')
 
 const initPokesIndexTabs = () => {
   tabs.forEach((tab) => {
     tab.addEventListener('click', (event) => {
       if (!(tab.classList.contains('active'))) {
-        tabs.forEach((tab) => {
-          tab.classList.toggle('active');
-        });
-        pokeContainers.forEach((pokeContainer) => {
-          pokeContainer.classList.toggle('active');
-        });
+        const activeTab = document.querySelector('.tab.active');
+        activeTab.classList.toggle('active');
+        event.target.classList.toggle('active');
+
+        const activeContainer = document.querySelector(`[data-anchor=${activeTab.dataset.target}]`)
+        const targetContainer = document.querySelector(`[data-anchor=${event.target.dataset.target}]`)
+        activeContainer.classList.toggle('active');
+        targetContainer.classList.toggle('active');
       }
     });
   });
