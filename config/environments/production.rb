795d7488164cfb8689f4dc5eeae5d94f412d0e7b
Einar Jonsson <einarjonsson@mac.com>
2019-03-20 11:25:29 +0100

    finished config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,9 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.action_cable.url = "wss://www.poke-app-einarj03.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [ "poke-app-einarj03.herokuapp.com" ]
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
