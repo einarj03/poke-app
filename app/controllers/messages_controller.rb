e4c163c937774a3d5956c15a1b9f055fec89ece7
Einar Jonsson <einarjonsson@mac.com>
2019-03-21 16:56:02 +0100

    fixed bug in messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController
   def create
+    @message = Message.create(message_params)
     @message.seen = true
     @message.save!
-    @message = Message.create(message_params)
     if @message.save
       respond_to do |format|
         format.html { redirect_to poke_path(@message.poke) }
