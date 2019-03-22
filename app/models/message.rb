32381dd78f684f8e80c52ef8b6c896759a96a780
Darrick Ervin II <sherwood_3494@yahoo.com>
2019-03-22 10:30:47 +0100

    creating action cable for notifications

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :poke
   belongs_to :user
   validates :content, presence: true
-  after_create :broadcast_message
+  after_create :broadcast_message, :broadcast_notification
 
   def broadcast_message
     ActionCable.server.broadcast("chat_room_#{poke.id}", {
@@ -10,6 +10,12 @@ class Message < ApplicationRecord
         partial: "messages/message",
         locals: { message: self, user_is_messages_author: false }),
       current_user_id: user.id
+    })
+  end
+
+  def broadcast_notification
+    ActionCable.server.broadcast("notifications_#{poke.id}", {
+      sender_id: user.id
     })
   end
 end
